350a16be1a732be472deda8e1fff99469cf2d0b6
charlesojihoji <charlesojihoji@gmail.com>
2025-12-06 13:01:54 -0500

    Added important features to the Courses Microservice

--- src/main/java/edu/uoengland/courses/service/CourseService.java
+++ src/main/java/edu/uoengland/courses/service/CourseService.java
@@ -1,5 +1,21 @@
 package edu.uoengland.courses.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import edu.uoengland.courses.dto.CourseDTO;
+import edu.uoengland.courses.entity.Course;
+
 public interface CourseService {
 
+	public Course createACourse(CourseDTO courseDTO);
+
+	public List<Course> getAllCourses();
+
+	public Optional<Course> getACourse(UUID courseId);
+
+	public void updateACourse(CourseDTO courseDTO);
+
+	public void deleteACourse(UUID courseId);
 }
--- src/main/java/edu/uoengland/courses/service/CourseServiceImpl.java
+++ src/main/java/edu/uoengland/courses/service/CourseServiceImpl.java
@@ -1,5 +1,63 @@
 package edu.uoengland.courses.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.uoengland.courses.dto.CourseDTO;
+import edu.uoengland.courses.entity.Course;
+import edu.uoengland.courses.repository.CoursesRepository;
+
+@Service
 public class CourseServiceImpl implements CourseService{
+
+	@Autowired
+	private CoursesRepository coursesRepository;
+	
+	@Override
+	public Course createACourse(CourseDTO courseDTO) {
+		
+		Course courseObject = new Course();
+		
+		courseObject.setCourseName(courseDTO.getCourseName());
+		courseObject.setCourseFacultyMember(courseDTO.getCourseFacultyMember());
+		courseObject.setInitialNumOfStudentsEnrolled(courseDTO.getInitialNumOfStudentsEnrolled());
+		
+		return coursesRepository.save(courseObject);
+	}
+
+	@Override
+	public List<Course> getAllCourses() {
+
+		return coursesRepository.findAll();
+	}
+
+	@Override
+	public Optional<Course> getACourse(UUID courseId) {
+
+		return coursesRepository.findById(courseId);
+	}
+
+	@Override
+	public void updateACourse(CourseDTO courseDTO) {
+
+		Course updatedCourse = new Course();
+		
+		updatedCourse.setCourseId(courseDTO.getCourseId());
+		updatedCourse.setCourseName(courseDTO.getCourseName());
+		updatedCourse.setCourseFacultyMember(courseDTO.getCourseFacultyMember());
+		updatedCourse.setInitialNumOfStudentsEnrolled(courseDTO.getInitialNumOfStudentsEnrolled());
+		
+		coursesRepository.save(updatedCourse);
+	}
+
+	@Override
+	public void deleteACourse(UUID courseId) {
+
+		coursesRepository.deleteById(courseId);
+	}
 
 }
